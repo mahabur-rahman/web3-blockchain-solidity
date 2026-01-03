8a11365ac4c7c91d8e5fb1b57d8aa566d44f9c5e
mahabur-rahman <annur4395@gmail.com>
2026-01-03 16:14:43 +0600

    feat: Setup ganache and web3 setup for testing and local deployment with truffle configure file

---
+++ truffle-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*"
+    }
+  },
+
+  contracts_directory: './src/contracts',
+  contracts_build_directory: './src/abis',
+  migrations_directory: './migrations',
+
+  compilers: {
+    solc: {
+      version: "0.8.21",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200
+        }
+      }
+    }
+  }
+};
